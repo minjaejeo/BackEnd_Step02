925f4097092c6113ba1b8ba8439794d946db2887
NCP08 <navercloudplatform08.gmail.com>
2024-04-12 15:20:17 +0900

    254page까지 수업 내용

--- springex/src/main/java/org/zerock/springex/sample/SampleService.java
+++ springex/src/main/java/org/zerock/springex/sample/SampleService.java
@@ -3,6 +3,7 @@ package org.zerock.springex.sample;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,7 @@ public class SampleService {
 
     // lombok의 @RequiredArgsConstructor을 사용해서 '생성자 주입 방식'으로 객체를 주입한다.
     // 반드시 final로 선언해야 한다.
+    @Qualifier("normal")
     private final SampleDAO sampleDAO;
 
     // 필드 주입 방식
--- springex/src/test/java/org/zerock/springex/sample/SampleTests.java
+++ springex/src/test/java/org/zerock/springex/sample/SampleTests.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 /*
 ServletContext : tomcat의 전역영역, tomcat의 Container가 관리하는 영역
 ApplicationContext : Spring Container 가 관리하는 영역, 이곳에 bean(관리되어지는 객체)를 저장한다.
@@ -25,9 +28,20 @@ public class SampleTests {
     @Autowired
     private SampleService sampleService;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     public void testService1(){
         log.info(sampleService);
         Assertions.assertNotNull(sampleService);
+    }
+    @Test
+    public void testConnection()throws Exception{
+        Connection connection = dataSource.getConnection();
+        log.info(connection);
+        Assertions.assertNotNull(connection);
+
+        connection.close();
     }
 }
