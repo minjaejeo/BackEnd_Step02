2be71de513f1ec3f51b566944805b29c5e08b59d
NCP08 <navercloudplatform08.gmail.com>
2024-04-12 13:04:58 +0900

    page 237 어노테이션 주입 완료

--- springex/src/test/java/org/zerock/springex/sample/SampleTests.java
+++ springex/src/test/java/org/zerock/springex/sample/SampleTests.java
@@ -1,4 +1,33 @@
 package org.zerock.springex.sample;
 
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/*
+ServletContext : tomcat의 전역영역, tomcat의 Container가 관리하는 영역
+ApplicationContext : Spring Container 가 관리하는 영역, 이곳에 bean(관리되어지는 객체)를 저장한다.
+ */
+
+@Log4j2
+@ExtendWith(SpringExtension.class)  // junit4 @Runwith
+
+// root-context.xml을 읽어들여서 ApplicationContext영역에 bean들을 생성한다.
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
 public class SampleTests {
+
+    // ApplicationContext에서 동일한 type을 찾아서 자동으로 주입해라.
+    // 필드 주입 방식
+    @Autowired
+    private SampleService sampleService;
+
+    @Test
+    public void testService1(){
+        log.info(sampleService);
+        Assertions.assertNotNull(sampleService);
+    }
 }
