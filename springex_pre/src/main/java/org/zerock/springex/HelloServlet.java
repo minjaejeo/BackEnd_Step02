b0b10d7dd4d01d2f9cf4fa0bdb89bed62866c24a
minjaejeo <157557014+minjaejeo@users.noreply.github.com>
2024-04-15 23:35:49 +0900

    HelloServlet, TimeMapper2.xml, servlet-context.xml, web.xml 까지 복습

--- springex_pre/src/main/java/org/zerock/springex/HelloServlet.java
+++ springex_pre/src/main/java/org/zerock/springex/HelloServlet.java
@@ -1,27 +1,31 @@
 package org.zerock.springex;
 
-import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="helloServlet", value="/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
+    public void init(){message = "Hello World!";}
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    public  void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
+        //Hello
+        PrintWriter out = res.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
+    public void destroy(){
 
-    public void destroy() {
     }
+
 }
