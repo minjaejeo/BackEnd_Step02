21f02409f1850f104db34ddb50a20565dd1d0c3b
NCP08 <navercloudplatform08.gmail.com>
2024-04-19 18:06:08 +0900

    4.19 수업 내용 459page까지

--- b01/src/main/java/org/zerock/b01/repository/BoardRepository.java
+++ b01/src/main/java/org/zerock/b01/repository/BoardRepository.java
@@ -1,7 +1,12 @@
 package org.zerock.b01.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.zerock.b01.domain.Board;
+import org.zerock.b01.repository.search.BoardSearch;
+
 /*
 인터페이스를 만들고
 JpaRepository를 상속받는데, 
@@ -16,5 +21,19 @@ private BoardRepository boardRepository;
 
 나 생성자 주입을 통해서 사용하면 된다.
  */
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, BoardSearch {
+
+    // 1) 쿼리메소드
+    //  https://spring.io/projects/spring-data-jpa
+
+    // 2) MySQL/MariaDB에 적합한 sql - NativeQuery
+    @Query(value = "select now()", nativeQuery = true)
+    String getTime();
+
+    // 3) JPQL - JPA에서 정한 표준 SQL, JPA에서 사용하는 Hiberate에서 NativeQuery로 자동 변환해서
+    // DBMS에 전달한다.
+    @Query("select b from Board b where b.title like concat('%', :keyword, '%')")
+    Page<Board> findKeyword(String keyword, Pageable pageable);
+
+    // 4) QueryDsl - 프로그래밍(객체, 메서드 호출) -> JPQL -> Native Query -> DBMS로 전달
 }
---
+++ b01/src/main/java/org/zerock/b01/repository/search/BoardSearch.java
@@ -0,0 +1,25 @@
+package org.zerock.b01.repository.search;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.zerock.b01.domain.Board;
+
+/*
+QueryDsl을 사용하기 위해서
+그런데 왜 필요하느냐?
+여기에 메서드를 정의하고 2군데 상속시킨다.
+1) 메서드에 QueryDsl문법으로 구현해야 하는 자식 클래스
+2) 우리가 Dao로 사용하는 BoardRepository에 상속시킨다.
+
+이렇게 하면 JPA가 BoardRepository에 상속시킨 BoardSearch를
+인지하고, 이 인터페이스를 기준으로 아래 명명법으로
+구현 클래스를 찾는다.(반드시 BoardSearchImpl= BoardSearch + Impl)
+찾은 다음에 BoardRepository에서 상속받은 추상메서드를 구현할 때
+BoardSearchImpl의 구현메서드를 호출하게 된다.
+ */
+public interface BoardSearch {
+
+    Page<Board> search1(Pageable pageable);
+
+    Page<Board> searchAll(String[] types, String keyword, Pageable pageable);
+}
