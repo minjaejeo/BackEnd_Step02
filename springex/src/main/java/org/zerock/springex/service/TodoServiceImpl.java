44cdcc8bafba395c6532bb0262792322ca42543b
minjaejeo <157557014+minjaejeo@users.noreply.github.com>
2024-04-19 09:31:57 +0900

    TodoService, TodoServiceImpl, root-context.xml 복습완료 및  servlet-context.xml 할 차례, web.xml 할 차례

--- springex/src/main/java/org/zerock/springex/service/TodoServiceImpl.java
+++ springex/src/main/java/org/zerock/springex/service/TodoServiceImpl.java
@@ -9,6 +9,7 @@ import org.zerock.springex.dto.PageRequestDTO;
 import org.zerock.springex.dto.PageResponseDTO;
 import org.zerock.springex.dto.TodoDTO;
 import org.zerock.springex.mapper.TodoMapper;
+import sun.jvm.hotspot.debugger.Page;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,13 +18,11 @@ import java.util.stream.Collectors;
 @Log4j2
 @RequiredArgsConstructor
 public class TodoServiceImpl implements TodoService {
-
     /*
     lombok이 제공하는 @RequiredArgsConstructor에 의해
     생성자가 생성되고 Spring Container에서
     final로 된 타입과 일치하는 bean을 찾아서 주입한다.
      */
-
     private final TodoMapper todoMapper;
 
     private final ModelMapper modelMapper;
@@ -34,25 +33,28 @@ public class TodoServiceImpl implements TodoService {
 
         // dto -> vo
         TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
+
         log.info(todoVO);
 
         // Dao에 해당하는 todoMapper를 사용하여 DBMS와 통신한다.
         todoMapper.insert(todoVO);
     }
 
+
     // 사용하지 않는다.
     public List<TodoDTO> getAll() {
         List<TodoDTO> dtoList = todoMapper.selectAll().stream()
                 .map(vo -> modelMapper.map(vo, TodoDTO.class))
                 .collect(Collectors.toList());
-
         return dtoList;
     }
 
     @Override
     public TodoDTO getOne(Long tno) {
         TodoVO todoVO = todoMapper.selectOne(tno);
+
         TodoDTO todoDTO = modelMapper.map(todoVO, TodoDTO.class);
+
         return todoDTO;
     }
 
@@ -62,20 +64,17 @@ public class TodoServiceImpl implements TodoService {
     }
 
     @Override
-    public void modify(TodoDTO todoDTo) {
-        TodoVO todoVO = modelMapper.map(todoDTo, TodoVO.class);
+    public void modify(TodoDTO todoDTO) {
+        TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
 
         todoMapper.update(todoVO);
-
     }
-
     @Override
     public PageResponseDTO<TodoDTO> getList(PageRequestDTO pageRequestDTO) {
         List<TodoVO> voList = todoMapper.selectList(pageRequestDTO);
         List<TodoDTO> dtoList = voList.stream()
                 .map(vo -> modelMapper.map(vo, TodoDTO.class))
                 .collect(Collectors.toList());
-
         int total = todoMapper.getCount(pageRequestDTO);
 
         PageResponseDTO<TodoDTO> pageResponseDTO = PageResponseDTO.<TodoDTO>withAll()
